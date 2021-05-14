b99c54a76965a67b56f35f3be7c78aaf044d259d
davidax0204 <davidax5625@gmail.com>
2021-05-14 17:20:44 +0300

    HomePage done

---
+++ public/js/menu-button.js
@@ -0,0 +1,15 @@
+const menuButton = document.getElementsByClassName("main-menu")
+menuButton[0].addEventListener("click",()=>
+{
+    myFunction()
+})
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ require('./db/mongoose')
 const userRouter = require('../src/routers/user')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.json())
 app.use(userRouter)
-
-const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
+
 
 module.exports = app

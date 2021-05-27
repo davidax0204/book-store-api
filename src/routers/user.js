3e7e29b36e5ab671c2744e8213dc28b48b7e5ac6
davidax0204 <davidax5625@gmail.com>
2021-05-27 16:41:24 +0300

    Admin-user part done

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('../models/user')
 const {auth,authAdmin} = require('../middleware/auth')
 const url = require('url')
+const { log } = require('console')
 // const { Console } = require('console')
 const router = new express.Router()
 
@@ -24,12 +25,58 @@ router.get('/sign-up', async (req,res)=>
     res.render('sign-up')
 })
 
-router.get('/profile/admin', authAdmin, (req,res)=>
-{
+// router.get('/profile/admin', authAdmin, (req,res)=>
+// {
 
     
-    res.render('admin-dashboard')
+//     res.render('admin-dashboard')
+// })
+
+router.get('/profile/admin/users',authAdmin, async (req,res)=>
+{
+    const users = await User.find({'adminTokens':{$size:0}})
+
+
+    res.render('admin-users-page',{
+        users:users
+    })
 })
+
+
+// router.post('/profile/admin/users', authAdmin, async (req,res)=>
+// {
+//     try 
+//     {
+//         const users = await User.find({})
+//         console.log('here?')
+//         res.redirect('/profile/admin/users')
+
+//         res.redirect(url.format({
+//             pathname:"/profile/admin/users",
+//             query: {
+//                 errorMSG: finalError
+//                 }
+//             }));
+//     } 
+//     catch (e) 
+//     {
+//         res.status(500).send()
+//     }
+//     // res.redirect('/profile/admin')
+// })
+
+
+
+// router.get('/profile/admin/books',authAdmin, (req,res)=>
+// {
+//     // res.render('admin-dashboard')
+// })
+
+// router.post('/profile/admin/books',authAdmin, (req,res)=>
+// {
+//     res.redirect('/profile/admin/books')
+// })
+
 
 router.get('/profile',auth, async (req,res)=>
 {
@@ -334,6 +381,226 @@ router.post('/profile/update', auth, async (req, res) =>
         //      }
         //   }));
         // res.send(e.errors.name.properties.message)
+    }
+})
+
+router.get('/profile/adminUpdate/user', authAdmin, async(req,res)=>
+{
+    console.log(req.query)
+    const user = await User.findOne({_id:req.query.user})
+
+    if(!req.query.errorMSG)
+    {
+        res.render('admin-user-update',{
+            name:user.name,
+            email:user.email,
+            id:user.id
+        })
+    }
+
+    if(req.query.errorMSG==='Email is Invalid')
+        {
+            res.render('admin-user-update',{
+                name:user.name,
+                email:user.email,
+                id:user.id,
+                emailErrorMSG:'Email is Invalid'
+            })
+        }
+        else if(req.query.errorMSG==='Email is taken')
+        {
+            res.render('admin-user-update',{
+                name:user.name,
+                email:user.email,
+                id:user.id,
+                emailErrorMSG:'Email is taken'
+            })
+        }
+        else if(req.query.errorMSG==='Name is Invalid')
+        {
+            res.render('admin-user-update',{
+                name:user.name,
+                email:user.email,
+                id:user.id,
+                nameErrorMSG:'Name is Invalid'
+            })
+        }
+        else if(req.query.errorMSG==='Password is Invalid')
+        {
+            res.render('admin-user-update',{
+                name:user.name,
+                email:user.email,
+                id:user.id,
+                passwordErrorMSG:'Password is Invalid'
+            })
+        }
+
+    
+})
+
+router.post('/profile/adminUpdate/user/:id', authAdmin, async(req,res)=>
+{
+    
+    const updates = Object.keys(req.body)
+    // console.log(updates)
+    const allowedUpdates = ['name', 'email', 'password']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    
+    if (!isValidOperation) 
+    {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+    
+    try 
+    {
+        const user = await User.findOne({_id:req.params.id})
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+        res.redirect(url.format({
+            pathname:"/profile/adminUpdate/user",
+            query: {
+                user:req.params.id
+            }
+        }));
+
+    } catch (e) 
+    {
+        // res.send(e)
+        const error = e.message.substr(24)
+        const finalError = ''
+        // res.send(error)
+        try
+        {
+            if(error[0] === 'e')
+            {
+                res.redirect(url.format({
+                    pathname:"/profile/adminUpdate/user",
+                    query: {
+                        errorMSG: 'Email is Invalid',
+                        user:req.params.id
+                    }
+                }));
+            }
+            else if(error[0] === 'n')
+            {
+                res.redirect(url.format({
+                    pathname:"/profile/adminUpdate/user",
+                    query: {
+                        errorMSG: 'Name is Invalid',
+                        user:req.params.id
+                    }
+                }));
+            }
+            else if(e.errors.password)
+            {
+                res.redirect(url.format({
+                    pathname:"/profile/adminUpdate/user",
+                    query: {
+                    errorMSG:'Password is Invalid',
+                    user:req.params.id
+                    }
+                }));
+            }
+        }
+        catch(e2)    
+        {
+            res.redirect(url.format({
+                pathname:"/profile/adminUpdate/user",
+                query: {
+                errorMSG: 'Email is taken',
+                user:req.params.id
+                }
+            }));
+        }    
+
+
+        // res.redirect(url.format({
+        //     pathname:"/profile",
+        //     query: {
+        //        errorMSG: finalError
+        //      }
+        //   }));
+        // res.send(e.errors.name.properties.message)
+    }
+})
+
+router.post('/profile/adminUpdate/user/delete/:id',authAdmin, async (req,res)=>
+{
+    console.log('here')
+    try
+    {
+        const user = await User.findOneAndDelete({'_id':req.params.id})
+        console.log(user)
+        res.redirect('/profile/admin/users')
+    }
+    catch(e)
+    {   
+        res.redirect(url.format({
+            pathname:"/profile/adminUpdate/user",
+            query: {
+                user:req.params.id
+            }
+        }));
+    }
+})
+
+router.post('/profile/adminUpdate/:id', authAdmin, async (req, res) => 
+{
+
+    try 
+    {
+        res.redirect(url.format({
+            pathname:"/profile/adminUpdate/user",
+            query: {
+                user:req.params.id
+            }
+        }));
+
+    } catch (e) 
+    {
+        // res.send(e)
+        const error = e.message.substr(24)
+        const finalError = ''
+        // res.send(error)
+        try
+        {
+            if(error[0] === 'e')
+            {
+                res.redirect(url.format({
+                    pathname:"/profile",
+                    query: {
+                    errorMSG: 'Email is Invalid'
+                    }
+                }));
+            }
+            else if(error[0] === 'n')
+            {
+                res.redirect(url.format({
+                    pathname:"/profile",
+                    query: {
+                    errorMSG: 'Name is Invalid'
+                    }
+                }));
+            }
+            else if(e.errors.password)
+            {
+                res.redirect(url.format({
+                    pathname:"/profile",
+                    query: {
+                    errorMSG: 'Password is Invalid'
+                    }
+                }));
+            }
+        }
+        catch(e2)    
+        {
+            res.redirect(url.format({
+                pathname:"/profile",
+                query: {
+                errorMSG: 'Email is taken'
+                }
+            }));
+        }    
     }
 })
 
